dafbe60ed5f66d654c2b0acb5189e328a9d76895
the-real-sec <glanloyand@gmail.com>
2024-10-22 15:09:10 +0530

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from re import DEBUG
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return "Hello ,world"
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0',debug=True)
